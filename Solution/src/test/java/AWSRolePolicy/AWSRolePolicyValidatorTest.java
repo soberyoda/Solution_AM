af4b13d41454f6223bbbc14117e0d38efee7bb25
Agata Marczyk <agamarczyk.agh@gmail.com>
2024-04-15 17:53:28 +0200

    added: test - AWSRolePolicyValidator

--- Solution/src/test/java/AWSRolePolicy/AWSRolePolicyValidatorTest.java
+++ Solution/src/test/java/AWSRolePolicy/AWSRolePolicyValidatorTest.java
@@ -1,7 +1,34 @@
 package AWSRolePolicy;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 
-class AWSRolePolicyValidatorTest {
+import Interfaces.JsonValidator;
+import Interfaces.PolicyNameValidator;
+import org.junit.Test;
 
+public class AWSRolePolicyValidatorTest {
+
+    @Test
+    public void testValidPolicy() {
+        AWSRolePolicyValidator validator = new AWSRolePolicyValidator(new AWSRolePolicyJsonValidator(), new AWSRolePolicyNameValidator());
+        assertTrue(validator.validate("D:\\AWSIAM_Internship_2024\\Solution\\src\\test\\java\\resources\\validJson.json"));
+    }
+
+    @Test
+    public void testInvalidJson() {
+        AWSRolePolicyValidator validator = new AWSRolePolicyValidator(new AWSRolePolicyJsonValidator(), new AWSRolePolicyNameValidator());
+        assertFalse(validator.validate("D:\\AWSIAM_Internship_2024\\Solution\\src\\test\\java\\resources\\invalidJson.json"));
+    }
+
+    @Test
+    public void testInvalidPolicyName() {
+        AWSRolePolicyValidator validator = new AWSRolePolicyValidator(new AWSRolePolicyJsonValidator(), new AWSRolePolicyNameValidator());
+        assertFalse(validator.validate("D:\\AWSIAM_Internship_2024\\Solution\\src\\test\\java\\resources\\invalidPolicyName.json"));
+    }
+
+    @Test
+    public void testWildcardResource() {
+        AWSRolePolicyValidator validator = new AWSRolePolicyValidator(new AWSRolePolicyJsonValidator(), new AWSRolePolicyNameValidator());
+        assertFalse(validator.validate("D:\\AWSIAM_Internship_2024\\Solution\\src\\test\\java\\resources\\policyWithWildCard.json"));
+    }
 }
